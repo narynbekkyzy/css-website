adbb5985e65173b2a6148fa8fd88e3a90bd4cbf7
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2022-03-06 15:31:28 -0500

    Implement the body container for the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-
+import './App.css'
+import MainPageImg from './assets/images/mainpage_img.svg'
 
 const App: React.FC = () => {
   return (
-    <div>
-      Hello World!
+    <div className='MainPage'>
+      <div className='MainPage-BodyContainer'>
+        <div className='MainPage-BodyContainer-Left'>
+          <h1>
+            Computer Science Society
+          </h1>
+          <h2>
+            It’s time to code 
+            to change the world.
+          </h2>
+          <div className='MainPage-BodyContainer-Left-ButtonsContainer'>
+            <button>Mission</button>
+            <button>Team</button>
+            <button>Events</button>
+            <button>Projects</button>
+            <button>Opportunities</button>
+          </div>
+        </div>
+
+        <div className='MainPage-BodyContainer-Right'>
+            <img src={MainPageImg}>
+            </img>
+        </div>
+      </div>
+      <div className='MainPage-FooterContainer'>
+
+      </div>
+      
    
     </div>
   )
