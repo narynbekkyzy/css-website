5fd2b8ab87e18841ccda8e279707ff4d6710eff2
FatimaOrtega <75735876+FatimaOrtega@users.noreply.github.com>
2022-03-06 15:31:08 -0500

    Set up HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import MainPage from './assets/mainpage_img.svg'
+import './App.css'
 
 
 const App: React.FC = () => {
   return (
-    <div>
-      Hello World!
-   
+    <div className='MainPage'>
+     <div className='MainPage-BodyContainer'>
+      <div className='MainPage-BodyContainer-Left'>
+        <h1> 
+          Computer Science Society
+        </h1>
+
+        <p>It's time to code to change the world</p>
+        
+        <div className='MainPage-BodyContainer-Left-ButtonsContainer'>
+          <button>Mission</button>
+          <button>Team</button>
+          <button>Events</button>
+          <button>Projects</button>
+          <button>Opportunities</button>
+          </div>
+        </div>
+
+        <div className='MainPage-BodyContainer-Right'>
+
+    <img src={MainPage}/>
+        </div>
+
+       </div>
+    <div className='MainPage-FooterContainer'>
+
+      </div>
+
+
     </div>
   )
 }
