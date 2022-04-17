ef09b0848f092b553b51ff2d063eb2ca2b83e56b
FatimaOrtega <75735876+FatimaOrtega@users.noreply.github.com>
2022-04-17 15:39:55 -0400

    Almost done with main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import MainPage from './assets/mainpage_img.svg'
+import Blob from './assets/blob.svg'
 import './App.css'
 
 
@@ -9,17 +10,17 @@ const App: React.FC = () => {
      <div className='MainPage-BodyContainer'>
       <div className='MainPage-BodyContainer-Left'>
         <h1> 
-          Computer Science Society
+        <span>C</span>omputer <span>S</span>cience <span>S</span>ociety
         </h1>
 
         <p>It's time to code to change the world</p>
         
         <div className='MainPage-BodyContainer-Left-ButtonsContainer'>
-          <button>Mission</button>
-          <button>Team</button>
-          <button>Events</button>
-          <button>Projects</button>
-          <button>Opportunities</button>
+          <button>&nbsp;Mission&nbsp;</button>
+          <button>&nbsp;Team&nbsp;</button>
+          <button>&nbsp;Events&nbsp;</button>
+          <button>&nbsp;Projects&nbsp;</button>
+          <button>&nbsp;Opportunities&nbsp;</button>
           </div>
         </div>
 
@@ -33,7 +34,7 @@ const App: React.FC = () => {
 
       </div>
 
-
+<img className="MainPage-Blob" src={Blob}></img>
     </div>
   )
 }
