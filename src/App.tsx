a2b4b69f2bb5afd780e6378009bd469ee0e8b14f
tachau03 <taminhchau8903@gmail.com>
2022-05-01 14:17:29 -0400

    MainPage for smaller screen

--- src/App.tsx
+++ src/App.tsx
@@ -12,21 +12,41 @@ const App: React.FC = () => {
       <div className='MainPage-BodyContainer'>
         <div className='MainPage-BodyContainer-Left'>
           <h1>
+<<<<<<< Updated upstream
             <span>C</span>omputer&nbsp;
             <span>S</span>cience&nbsp;
             <span>S</span>ociety&nbsp;
+=======
+            <h1><span>C</span>omputer&nbsp;</h1>
+            <h1><span>S</span>cience&nbsp;</h1>
+            <h1><span>S</span>ociety</h1>
+>>>>>>> Stashed changes
           </h1>
           <h2>
             It's time to code 
             to change the world.
           </h2>
           <div className='MainPage-BodyContainer-Left-ButtonsContainer'>
+<<<<<<< Updated upstream
           <button>&nbsp;Mission&nbsp;</button>
           <button>&nbsp;Team&nbsp;</button>
           <button>&nbsp;Events&nbsp;</button>
           <button>&nbsp;Projects&nbsp;</button>
           <button>&nbsp;Opportunities&nbsp;</button>
         </div>
+=======
+            <button>&nbsp;Mission&nbsp;</button>
+            <button>&nbsp;Team&nbsp;</button>
+            <button>&nbsp;Events&nbsp;</button>
+            <button>&nbsp;Projects&nbsp;</button>
+            <button>&nbsp;Opportunities&nbsp;</button>
+          </div>
+          <div id = "Mobile-Footer" className='MainPage-FooterContainer'>
+          <img id='logo' src={Logo}></img>
+          <img id='dc' src={Dc}></img>
+          <img id='ig' src={Ig}></img>
+      </div>
+>>>>>>> Stashed changes
         </div>
 
         <div className='MainPage-BodyContainer-Right'>
@@ -34,7 +54,17 @@ const App: React.FC = () => {
           </img>
         </div>
       </div>
+<<<<<<< Updated upstream
       <div className='MainPage-FooterContainer'>
+=======
+      <div id = "Desktop-Footer" className='MainPage-FooterContainer'>
+          <img id='logo' src={Logo}></img>
+          <img id='dc' src={Dc}></img>
+          <img id='ig' src={Ig}></img>
+      </div>
+      
+      <img className="MainPage-Blob" src={Blob}></img>
+>>>>>>> Stashed changes
 
       </div>
     
