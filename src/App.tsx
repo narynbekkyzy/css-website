fbc0b1ebaf3a22387ece6c86ea171ba481eaa49d
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2022-07-20 00:18:39 -0400

    Add all fonts, colors and our logos

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={'images/logos/circleLogo.png'} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <h1 className='Montserrat'>Montserrat Font</h1>
+        <h1 className='Inter'>Inter Font</h1>
+        <h1 className='Aurebesh'>Aurebesh Font</h1>
+        <h1 className='Aurebesh-English'>Aurebesh-English Font</h1>
+        <br></br>
         <a
           className="App-link"
           href="https://reactjs.org"
