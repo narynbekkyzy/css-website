79701450388bc094159bf1d753726836634f34fa
mpukabaofficial <99944714+mpukabaofficial@users.noreply.github.com>
2022-07-20 13:08:08 -0400

    card typography

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={'images/logos/circleLogo.png'} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <h1 className='Montserrat'>Montserrat Font</h1>
-        <h1 className='Inter'>Inter Font</h1>
-        <h1 className='Aurebesh'>Aurebesh Font</h1>
-        <h1 className='Aurebesh-English'>Aurebesh-English Font</h1>
-        <br></br>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
