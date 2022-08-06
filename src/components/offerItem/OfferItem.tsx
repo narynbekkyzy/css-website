e404d35f3e69cba3071a91b0d109b52d573f6473
tachau03 <taminhchau8903@gmail.com>
2022-08-06 18:49:34 +0700

    First OfferItem and WhatWeOffer commit

--- src/components/offerItem/OfferItem.tsx
+++ src/components/offerItem/OfferItem.tsx
@@ -1,15 +1,19 @@
 import './OfferItem.css'
 import data from "../../content/header_footer.json";
+import { isPropertySignature } from 'typescript';
 
 interface Item {
-    text: string;
+    text?: string;
     image: string;
 }
 
 export function OfferItem(props: Item): JSX.Element  {
     return (
-        <img
-            id="Mentorship">
-        </img>
+        <div className={props.image}>
+            <img  
+                src={props.image} >
+            </img>
+            <p>{props.text}</p>
+        </div>
     )
 }
---
+++ src/pages/main/sections/WhatWeOffer.tsx
@@ -0,0 +1,29 @@
+import './WhatWeOffer.css'
+import data from "../../../content/what_we_offer.json";
+import { Title } from '../../../components/titles/Title';
+import { OfferItem } from '../../../components/offerItem/OfferItem';
+
+export function WhatWeOffer(): JSX.Element {
+    return (
+        <div className='WhatWeOffer'>
+            <div className='WhatWeOffer-Container'>
+                <Title 
+                name = {'Primary'}
+                styling = {'MiddleBlack'}
+                text = {'What we offer'}/>
+                <div className='WhatWeOffer-Row'>
+                    {data.items.map((item, index) => {
+                        return(
+                            <a href={item.link} target="_blank" rel="noreferrer noopener">
+                                {<OfferItem 
+                                    image={item.img}
+                                    text={item.name}/>
+                                }
+                            </a>
+                        )
+                    })}
+                </div>
+            </div>
+        </div>
+    )
+}
