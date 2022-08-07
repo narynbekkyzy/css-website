276325e16770eaac51cbfbc294cfff13c7eb50c0
Ronan Wolfe <ronangwolfe@gmail.com>
2022-08-07 11:08:57 -0700

    Added option to attach an image to textContainers.

--- src/components/textContainer/textContainer.tsx
+++ src/components/textContainer/textContainer.tsx
@@ -4,32 +4,36 @@ import './textContainer.css'
 import { Title } from '../titles/Title'
 
 interface TextContainer {
-    title?: React.ReactNode;
-    titleName?: string;
+    isPrimary?: boolean;
     titleText?: string;
     desc?: string;
-    color?: string;
-    direction?: string;
+    white?: boolean;
+    right?: boolean;
+    img?: React.ReactNode;
+    imgFirst?: boolean;
     buttons?: Array<React.ReactNode>;
 }
 
 export function TextContainer(props: TextContainer): JSX.Element {
-    let dir = (props.direction ? 'right' : 'left');
-    let col = (props.color ? 'white' : 'black');
+    let dir = (props.right ? 'right' : 'left');
+    let col = (props.white ? 'white' : 'black');
     let btns = (props.buttons ? props.buttons.map(function (btn) { return btn }) : 0);
     let title = <Title
-        name={(props.titleName ? 'Primary' : "Main")}
-        style={props.titleName ?
+        name={(props.isPrimary ? 'Primary' : "Main")}
+        style={props.isPrimary ?
             dir.charAt(0).toUpperCase() + dir.slice(1) + col.charAt(0).toUpperCase() + col.slice(1) :
             col.charAt(0).toUpperCase() + col.slice(1)}
         text={props.titleText ? props.titleText : ""} />
-    return <div className={dir + col}>
-        {title}
-        {props.desc ? <p>
-            {props.desc}
-        </p> : <div style={{ height: "0px", width: "0px" }} />}
-        {btns != 0 ? <div className={"btns" + dir}>
-            {btns}
-        </div> : <div style={{ height: "0px", width: "0px" }} />}
+    return <div className="textContainer" style={props.imgFirst ? {flexDirection: "row-reverse", justifyContent: "flex-end"} : {} }>
+        <div style={props.img ? props.imgFirst ? { marginLeft: "45px" } : { marginRight: "45px"} : {}} className={dir + col}>
+            {title}
+            {props.desc ? <p>
+                {props.desc}
+            </p> : <div style={{ height: "0px", width: "0px" }} />}
+            {btns != 0 ? <div className={"btns" + dir}>
+                {btns}
+            </div> : <div style={{ height: "0px", width: "0px" }} />}
+        </div>
+        {props.img ? props.img : ""}
     </div>;
 }
