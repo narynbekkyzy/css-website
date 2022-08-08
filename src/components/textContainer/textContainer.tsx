1cba06ee0b7bd2b2df5696ab46dcda3260114ef2
Ronan Wolfe <ronangwolfe@gmail.com>
2022-08-08 13:09:19 -0700

    Documented the textContainer tsx & css files

--- src/components/textContainer/textContainer.tsx
+++ src/components/textContainer/textContainer.tsx
@@ -3,27 +3,34 @@ import './textContainer.css'
 
 import { Title } from '../titles/Title'
 
+//The interface defines the basic props that will be used by a textContainer object
+//All variables are optional, but isPrimary titleText will only work if both are used.
 interface TextContainer {
-    isPrimary?: boolean;
-    titleText?: string;
-    desc?: string;
-    white?: boolean;
-    right?: boolean;
-    img?: React.ReactNode;
-    imgFirst?: boolean;
-    buttons?: Array<React.ReactNode>;
+    isPrimary?: boolean;             //Determines whether the Title object will be "primary" or "main"
+    titleText?: string;              //The text used for primary title
+    desc?: string;                   //The body text
+    white?: boolean;                 //Set to true for white text
+    right?: boolean;                 //Set to true to align content to the right
+    img?: React.ReactNode;           //Image object to be used
+    imgFirst?: boolean;              //Set to true if the img should be first
+    buttons?: Array<React.ReactNode>;//Array of buttons
 }
 
 export function TextContainer(props: TextContainer): JSX.Element {
+    //Defines content alignment
     let dir = (props.right ? 'right' : 'left');
+    //Defines text color
     let col = (props.white ? 'white' : 'black');
+    //Returns all buttons
     let btns = (props.buttons ? props.buttons.map(function (btn) { return btn }) : 0);
+    //Create's title object
     let title = <Title
         name={(props.isPrimary ? 'Primary' : "Main")}
         styling={props.isPrimary ?
             dir.charAt(0).toUpperCase() + dir.slice(1) + col.charAt(0).toUpperCase() + col.slice(1) :
             col.charAt(0).toUpperCase() + col.slice(1)}
-        text={props.titleText ? props.titleText : ""} />
+        text={props.isPrimary && props.titleText ? props.titleText : ""} />
+    //Returns the textContainer object
     return <div className="textContainer" style={props.imgFirst ? {flexDirection: "row-reverse", justifyContent: "flex-end"} : {} }>
         <div style={props.img ? props.imgFirst ? { marginLeft: "45px" } : { marginRight: "45px"} : {}} className={dir + col}>
             {title}
