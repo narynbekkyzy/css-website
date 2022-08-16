fc50496fac2477c14acfbaabf45c0b02c12c35dd
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2022-08-15 20:59:07 -0400

    Add most of main page elements

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,10 +1,37 @@
 import "./Main.css";
 import { EventSection } from "./sections/eventsSection";
+import { WhatWeBuild } from "./sections/WhatWeBuild";
+import { WhoWeAre } from "./sections/WhoWeAre";
+
+import data from '../../content/main_page.json';
+import { WhatWeOffer } from "./sections/WhatWeOffer";
+import { Footer } from "../../components/Footer";
 
 export function MainPage(): JSX.Element {
   return (
     <>
+      <WhoWeAre 
+        desc={data.who_we_are.description} 
+        img={<img className="Image3by4" src={data.who_we_are.image}/>} 
+        buttons={[
+          <button className="default-white">Button 1</button>,
+          <button className="default-white">Button 2</button>
+        ]}/>
+
+      <WhatWeOffer 
+        title={data.what_we_offer.MyTitlte} 
+        items={data.what_we_offer.items}/>
+      
+      <WhatWeBuild 
+        desc={data.who_we_are.description} 
+        img={<img src={data.what_we_build.image}/>} 
+        buttons={[
+          <button className="default-white">Button 1</button>,
+          <button className="default-white">Button 2</button>
+        ]}
+      />
       <EventSection />
+      <Footer/>
     </>
   );
 }
--- src/pages/main/sections/WhoWeAre.tsx
+++ src/pages/main/sections/WhoWeAre.tsx
@@ -1,29 +1,32 @@
 import React from 'react';
 
-import { TextContainer } from './textContainer/textContainer'
+import { TextContainer } from '../../../components/textContainer/textContainer'
 
-interface WhatWeBuild {
+interface WhoWeAre {
     desc: string;                   //Body Text
-    img: React.ReactNode;           //Img of a project example (dVox)
+    img: React.ReactNode;           //Img of club members
     buttons: Array<React.ReactNode>;//Array of buttons
 }
 
-export function WhatWeBuild(props: WhatWeBuild): JSX.Element {
+export function WhoWeAre(props: WhoWeAre): JSX.Element {
     //Outer div is a container for center and establishing the background block
     //Inner div is for positioning the actual TextContainer
-    return <div className="WhatWeBuild-Container" style={{
+    return <div className="WhoWeAre-Container" style={{
         backgroundColor: "var(--black)", width: "100%",
         display: "flex", justifyContent: "center" 
     }}>
-        <div className="WhatWeBuild-Content" style={{
-            marginLeft: "219px", width: "fit-content"
+        <div className="WhoWeAre-Content" style={{
+            marginTop: "80px", marginBottom: "80px",
+            marginLeft: "219px", marginRight: "219px", width: "fit-content"
         }}>
             <TextContainer
                 isPrimary={true}
-                titleText={"What We Build"}
+                titleText={"Who We Are"}
                 desc={props.desc}
                 white={true}
+                right={true}
                 img={props.img}
+                imgFirst={true}
                 buttons={props.buttons}
             />
         </div>
