99f9d2e92810de72a5d7b339db4825ef423d0f7a
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2022-08-19 13:06:51 -0400

    Add Events page

--- src/components/event_card/EventCard.tsx
+++ src/components/event_card/EventCard.tsx
@@ -1,5 +1,6 @@
+import { motion, usePresence } from "framer-motion";
+import { useEffect } from "react";
 import "./EventCard.css";
-
 
 interface EventCardProps{
     title: string;
@@ -8,25 +9,40 @@ interface EventCardProps{
     date: string;
     img: string;
     type: string;
+    key: number;
 }
 
 
 export function EventCard(props: EventCardProps): JSX.Element {
 
+    const [isPresent, safeToRemove] = usePresence()
 
+    useEffect(() => {
+        !isPresent && setTimeout(safeToRemove, 1000)
+      }, [isPresent])
+
+      
     return(
-        <div className="EventCard">
-            <img src={props.img}/>
-            <h3 className="EventCard-H3 standard-padding">{props.title}</h3>
-            <p className="EventCard-P standard-padding">{props.description}</p>
-            <div className="standard-padding">
-                <h3 className="EventCard-H3">{props.date}</h3>
-                <h3 className="EventCard-H3">{props.location}</h3>
-            </div>
-            <div className="standard-padding">
-                <button className="FL-white">Add event to the calendar</button>
-            </div>
-            <div className="EventCard-TYPE" style={{"backgroundColor" : `var(--event-card-color-${props.type}`}}></div>
-        </div>       
+        <motion.div
+            key={props.title + props.key}
+            initial={{ scale: 0.8, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            exit={{ scale: 0.8, opacity: 0 }}
+            transition={{ type: "spring" }}
+        >
+            <div className="EventCard">
+                <img src={"/images/events/" + props.img}/>
+                <h3 className="EventCard-H3 standard-padding">{props.title}</h3>
+                <p className="EventCard-P standard-padding">{props.description}</p>
+                <div className="standard-padding">
+                    <h3 className="EventCard-H3">{props.date}</h3>
+                    <h3 className="EventCard-H3">{props.location}</h3>
+                </div>
+                <div className="standard-padding">
+                    <button className="FL-white">Add event to the calendar</button>
+                </div>
+                <div className="EventCard-TYPE" style={{"backgroundColor" : `var(--event-card-color-${props.type}`}}></div>
+            </div>       
+        </motion.div>
     )
 }
--- src/pages/events/Events.tsx
+++ src/pages/events/Events.tsx
@@ -1,19 +1,65 @@
-import './Events.css';
-import { EventCard } from '../../components/event_card/EventCard';
+import "./Events.css";
+import { EventCard } from "../../components/event_card/EventCard";
+import data from "../../content/events.json";
+import { Title } from "../../components/titles/Title";
+import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion"
 
 export function EventsPage(): JSX.Element {
-    return(
-        <>
-        <EventCard 
-            title={'Interview Experience'} 
-            description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Condimentum diam orci pretium a pharetra, feugiat cursus. Dictumst risus, sem egestas odio cras adipiscing vulputate. '} 
-            location={'OU312'} 
-            date={'April 16'} 
-            img={'/images/events/TeslaPoster.png'} 
-            type={'career'}
-        />
-            
-        </>
-    )
 
+    const [events, setEvents] = useState("");
+    const [allCards, setAllCards] = useState(data.events);
+
+    function filterAllCards() {
+        if (events === "") {
+            setAllCards(data.events);
+        } else {
+            setAllCards(data.events.filter(event => event.type === events));
+        }
+    }
+    
+    useEffect(() => {
+        filterAllCards();
+    } , [events]);
+
+  return (
+    <>
+      <div className="EventsContainer">
+        <Title 
+            text={data.title} 
+            name={"Primary"}
+            styling={"MiddleBlack"}
+        />
+    
+        <div className="EventsContainer-Categories">
+            <button onClick={() => setEvents("")}>all</button>
+            {data.categories.map((category, index) => {
+                return(
+                    <button onClick={() => setEvents(category)}>{category}</button>
+                )
+            })}
+        </div>
+        
+       
+        <div className="EventsContainer-Current">
+            {allCards.map((event, key) => {
+                return (
+                <AnimatePresence mode="wait">
+                    <EventCard
+                        key={key}
+                        title={event.title}
+                        description={event.description}
+                        location={event.location}
+                        date={event.date}
+                        img={event.img}
+                        type={event.type}
+                    />
+                </AnimatePresence>
+                );
+            })}
+        </div>
+
+      </div>
+    </>
+  );
 }
