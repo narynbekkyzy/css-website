c919253ed3854249df2ecea00ec3c18e95e3bdc3
tachau03 <taminhchau8903@gmail.com>
2022-08-21 21:32:06 +0700

    Update comments for StartMain and MeetTeam

--- src/pages/main/components/StartMain.tsx
+++ src/pages/main/components/StartMain.tsx
@@ -1,3 +1,10 @@
+/*
+    This file create the first part of CSS website.
+    Can be used by passing parameters:
+        + desc: text inside
+        + img: the image displayed in the right
+        + buttons: list of buttons under the desc (text)
+*/
 import React from 'react';
 import './StartMain.css'
 
@@ -5,17 +12,16 @@ import { TextContainer } from '../../../components/textContainer/textContainer'
 import { Header } from '../../../components/Header';
 
 interface StartMain {
-    desc: string;                   //Body Text
-    img: React.ReactNode;           //Img of club members
-    buttons: Array<React.ReactNode>;//Array of buttons
+    desc: string;                       //Body Text
+    img: React.ReactNode;               //Img of club members
+    buttons: Array<React.ReactNode>;    //Array of buttons
 }
 
 export function StartMain(props: StartMain): JSX.Element {
-    //Outer div is a container for center and establishing the background block
-    //Inner div is for positioning the actual TextContainer
-    return <div className="StartMain-Container">
-        <div className="StartMain-Content">
-            <TextContainer
+    return <div className="StartMain-Container">        {/* for center and establishing the background block */}
+        <div className="StartMain-Content">  
+            {/* for description, image and buttons */}           
+            <TextContainer                              
                 isPrimary={false}
                 desc={props.desc}
                 white={false}
--- src/pages/team/components/MeetTeam.tsx
+++ src/pages/team/components/MeetTeam.tsx
@@ -1,3 +1,11 @@
+/*
+    This file implemented the section "Meet Our Team" inside "Team" section on the header
+    Can be used by passing param:
+        + desc: text to describe 
+        + img: big image displaed in the center
+        + buttons: clickable buttons linked to other sections
+*/
+
 import React from 'react';
 import './MeetTeam.css'
 
@@ -5,27 +13,33 @@ import { TextContainer } from '../../../components/textContainer/textContainer'
 import { Title } from '../../../components/titles/Title';
 
 interface MeetTeam {
-    desc: string;                   //Body Text
+    desc?: string;                   //Body Text
     img: React.ReactNode;           //Img of club members
-    buttons: Array<React.ReactNode>;//Array of buttons
+    buttons?: Array<React.ReactNode>;//Array of buttons
 }
 
 export function MeetTeam(props: MeetTeam): JSX.Element {
-    //Outer div is a container for center and establishing the background block
-    //Inner div is for positioning the actual TextContainer
-    //Returns all buttons
+    /* map all passed button s*/
     let btns = (props.buttons ? props.buttons.map(function (btn) { return btn }) : 0);
 
-    return <div className="MeetTeam-Container">
+    return <div className="MeetTeam-Container"> {/* center and establishing the background block*/}
         <div className="MeetTeam-Content">
+            {/* Title box on the top */}
             <div className='Title-Box'><Title text="Meet our people" name = "Primary" styling="MiddleWhite"/></div>
+
+            {/* Image */}
             <a className="bigImage">{props.img}</a>
+
+            {/* The block containing desc and buttons */}
             <div className="Frame">
+                {/* Returns description if available*/}
                 {props.desc ? (
                     <p>{props.desc}</p>
                     ) : (
                     <div style={{ height: "0px", width: "0px" }} />
                     )}
+
+                {/* Returns all buttons if available*/}
                 {btns != 0 ? (
                     <div className="buttons">{btns}</div>
                     ) : (
