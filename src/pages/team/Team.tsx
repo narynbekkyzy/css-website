54640416b68aeff1f81ec8dce9be6d12f1a724b7
Fatima Ortega <fatimarortega@gmail.com>
2022-09-11 00:49:18 -0400

    Took off buttons on team page

--- src/pages/team/Team.tsx
+++ src/pages/team/Team.tsx
@@ -11,8 +11,7 @@ export function TeamPage(): JSX.Element {
         desc={data.who_we_are.description} 
         img={<img className="Image3by4" src={data.who_we_are.image}/>} 
         buttons={[
-          <button className="default-white">Button 1</button>,
-          <button className="default-white">Button 2</button>
+       
         ]}/>
         <OurMembers/>
     </>
