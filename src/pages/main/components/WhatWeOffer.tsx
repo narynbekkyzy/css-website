6d5e1126460d9c5b8317d707fc2137e8b563ddba
Ronan Wolfe <ronangwolfe@gmail.com>
2023-01-15 22:17:47 -0800

    Added more thorough documentation for What We Offer component.

--- src/pages/main/components/WhatWeOffer.tsx
+++ src/pages/main/components/WhatWeOffer.tsx
@@ -1,12 +1,15 @@
 /*
     This file implements WhatWeOffer section including:
-        - 'What we offer' title (pass through 'title' param from json file)
-        - OfferItems describing each part (contents through 'items' from json file)
+        -> interface MyTitle - 'What we offer' title (pass through 'title' param from json file)
+        -> interface MyTitle - OfferItems describing each part (contents through 'items' from json file)
+        -> interface WhatWeOffer - Parameters for WhatWeOffer section, which is comprised of previous two params
+        -> function WhatWeOffer - export function for main component
 */
 import './WhatWeOffer.css'
 import { Title } from '../../../components/titles/Title';
 import { OfferItem } from '../../../components/offerItem/OfferItem';
 
+// 'What we offer' title (pass through 'title' param from json file)
 interface MyItem {
     name: string;
     link: string;
@@ -14,25 +17,32 @@ interface MyItem {
     detail: string;
 }
 
-interface MyTitlte {
+// OfferItems describing each part (contents through 'items' from json file)
+interface MyTitle {
     name: string;
     styling: string;
     text: string;
 }
 
+// Parameters for WhatWeOffer section, which is comprised of previous two params
 interface WhatWeOffer{
-    title: MyTitlte;
+    title: MyTitle;
     items: Array<MyItem>;
 }
 
+// export function for main component
 export function WhatWeOffer(props: WhatWeOffer): JSX.Element {
     return (
+        // Outer 'Background' Component
         <div className='WhatWeOffer'>
+            {/** Inner 'Content' Component **/}
             <div className='WhatWeOffer-Container'>
+                {/** Header component **/}
                 <Title 
                 name = {props.title.name}
                 styling = {props.title.styling}
-                text = {props.title.text}/>
+                text={props.title.text} />
+                {/** Interactive "offer" items **/}
                 <div className='WhatWeOffer-Row'>
                     {props.items.map((item, index) => {
                         return(
