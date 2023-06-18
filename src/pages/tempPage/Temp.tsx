295c310191a82e3bb638ee68cbab1b17470871fd
bakp22 <101152896+bakp22@users.noreply.github.com>
2023-06-18 16:01:10 -0400

    Push Branch

--- src/pages/tempPage/Temp.tsx
+++ src/pages/tempPage/Temp.tsx
@@ -5,15 +5,22 @@
  */
 import { useEffect, useState } from "react";
 import './temp.css'
-
+function Add(x: any,y: any) {
+  return x+y;
+}
 /* Placeholder components that displays a message work work in progress pages
  * (or broken pages that need to be fixed but can't be done so immediately)
  */
+
+//this is a comment.
+
 export function TempPage(): JSX.Element { 
+  let z = Add(2,3);
   return (
     <>
       <div className="Tmp_body">
         <h1>Coming Soon</h1>
+        <h2>Hello World{z}</h2>
       </div>  
     </>
   );
